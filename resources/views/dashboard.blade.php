4beca10baa031f30fea1120e2a239a5105d066bf
Victor Arana Flores <victor@codersfree.com>
2023-09-29 11:40:16 -0500

    Actions

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
+            
+            {{-- @livewire('create-post', [
+                'title' => "Hola mundo pasado desde la vista",
+                'user' => 1
+            ]) --}}
+
+            @livewire('paises')
+
         </div>
     </div>
 </x-app-layout>
